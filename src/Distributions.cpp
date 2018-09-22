c1c54c46b7938ae61773cf2148d199cd4f99da77
Easton Huch <easton.huch@gmail.com>
2018-09-21 21:36:32 -0600

    started using sqrtmat_sympd

--- src/Distributions.cpp
+++ src/Distributions.cpp
@@ -18,10 +18,28 @@ arma::colvec mvnorm(const arma::colvec & mean, const arma::mat & Sigma) {
   for (int i = 0; i < n; ++i) {
     z.at(i) = R::rnorm(0.0, 1.0);
   }
-  arma::colvec x = mean + arma::chol(Sigma).t() * z;
+
+  // Calculate square root matrix of Sigma
+  arma::mat Sigma_sqrt;
+  try {
+    Sigma_sqrt = arma::sqrtmat_sympd(Sigma);
+  }
+  catch (std::runtime_error e)
+  {
+    Rcout << "Failed to calculate sqrt(Sigma) using sqrtmat_sympd" << std::endl;
+    Rcout << "Forcing symmetry using (Sigma + Sigma')/2 and trying again" << std::endl;
+    Rcout << Sigma << std::endl;
+    arma::mat Sigma_sym = ( Sigma + Sigma.t() ) / 2;
+    Rcout << Sigma_sym << std::endl;
+    arma::mat Sigma_sqrt = arma::sqrtmat_sympd(Sigma_sym);
+    Rcout << Sigma_sqrt << std::endl;
+  }
+
+  arma::colvec x = mean + Sigma_sqrt * z;
   return x;
 };
 
+// Should this be the scale or rate parameterization?
 double rigamma(const double a, const double scl) {
-  return (1 / R::rgamma(a, 1 / scl));
+  return (1 / R::rgamma(a, 1/scl));
 }
--- src/Sample.cpp
+++ src/Sample.cpp
@@ -58,21 +58,21 @@ void SampleSigma2(const double & alpha_sigma2, const double & beta_sigma2,
 }
 
 void SampleLambda(const double & alpha_lambda, const double & beta_lambda,
-                 const int & p, const int & T, int i,
-                 arma::mat & G, arma::cube & C,
-                 arma::cube & theta, arma::colvec & lambda) {
-  const double alpha_new = alpha_lambda + p * T / 2;
+                  const int & p, const int & T, int i,
+                  arma::mat & G, arma::cube & C,
+                  arma::cube & theta, arma::colvec & lambda) {
+  const double alpha_new = alpha_lambda + p*T/2;
   arma::mat P(p, p);
   arma::mat tmp(1, 1);
   double total = 0;
   for (int t = 1; t <= T; ++t) {
     P = G * C.slice(t - 1) * G.t();
-    arma::colvec x = theta.slice(i).col(t) - G * theta.slice(i).col(t - 1);
+    arma::colvec x = theta.slice(i).col(t) - G * theta.slice(i).col(t-1);
     tmp = (x.t() * solve(P, x));
     total += tmp(0);
   }
   const double beta_new = beta_lambda + total / 2;
-  lambda[i + 1] = rigamma(alpha_new, beta_new);
+  lambda[i+1] = rigamma(alpha_new, beta_new);
   return;
 }
 
