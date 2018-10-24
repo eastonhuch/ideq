f1df541bfbcfb2413b17137a7f566da948f744bc
Easton Huch <easton.huch@gmail.com>
2018-10-24 14:18:16 -0600

    Fixed indexing error in KalmanDiscount

--- src/Kalman.cpp
+++ src/Kalman.cpp
@@ -47,22 +47,21 @@ void Kalman(arma::mat & m, arma::cube & C, arma::mat & a, arma::cube & R_inv,
 void KalmanDiscount(arma::mat & m, arma::cube & C, arma::mat & a, arma::cube & R_inv,
                     const arma::mat & Y, const arma::mat & F, const arma::mat & G,
                     const double sigma2 , const double lambda) {
+  // This assumes that V is (sigma2 * I)
   const int T = Y.n_cols-1;
   const int S = Y.n_rows;
   const int p = G.n_rows;
 
-  // Don't need to keep these
+  // Don't need to keep these quantities
   arma::mat Q(S, S), Q_inv(S, S), R_t(p, p), FR(S, p), RF_t(p, S);
   arma::colvec f(S);
   // Could also save transposed copies of F, G
 
-  for (int t=1; t<T; ++t) {
+  for (int t = 1; t <= T; ++t) {
     checkUserInterrupt();
-
     // One step ahead predictive distribution of theta
     a.col(t) = G * m.col(t-1);
     R_t = (1 + lambda) * G * C.slice(t-1) * G.t();
-    make_symmetric(R_t);
 
     // One step ahead predictive distribution of Y_t
     f = F * a.col(t);
@@ -70,7 +69,6 @@ void KalmanDiscount(arma::mat & m, arma::cube & C, arma::mat & a, arma::cube & R
     Q = FR * F.t();
     Q.diag() += sigma2;
     Q_inv = arma::inv_sympd(Q);
-    make_symmetric(Q_inv);
 
     // Filtering distribution of theta
     RF_t = FR.t();
